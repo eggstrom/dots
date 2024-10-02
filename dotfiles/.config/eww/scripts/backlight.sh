8b0ea718ae2eb5a2fe2a245692b18b95b2c73c39
eggstrom <tristan65536@gmail.com>
2024-10-02 08:44:22 +0200

    Redirect backlight check stderr to /dev/null

--- dotfiles/.config/eww/scripts/backlight.sh
+++ dotfiles/.config/eww/scripts/backlight.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Prints "true" and updates brightness if backlight is found, otherwise prints "false"
-if brightnessctl -c backlight; then
+if brightnessctl -c backlight 2>/dev/null; then
     eww update brightness="$(("$(brightnessctl get)" * 100 / "$(brightnessctl max)"))"
     echo true
 else
