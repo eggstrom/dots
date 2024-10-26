b882c1b1d7a4a96a72255afe5960433e8e934175
eggstrom <tristan65536@gmail.com>
2024-10-26 07:04:08 +0200

    Make some TUIs blockier

--- dotfiles/.config/nvim/lua/plugins/misc.lua
+++ dotfiles/.config/nvim/lua/plugins/misc.lua
@@ -1,6 +1,12 @@
 require("nvim-autopairs").setup()
 
-require("lualine").setup()
+require("lualine").setup({
+    options = {
+        component_separators = { left = "|", right = "|" },
+        section_separators = { left = "", right = "" },
+    },
+})
+
 require("bufferline").setup({
     options = {
         always_show_bufferline = false,
--- home/btop.nix
+++ home/btop.nix
@@ -1,7 +1,12 @@
 {
   programs.btop = {
     enable = true;
-    settings.vim_keys = true;
+
+    settings = {
+      vim_keys = true;
+      rounded_corners = false;
+    };
+
     catppuccin.enable = true;
   };
 }
