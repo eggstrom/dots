76b6e3fafbdb28cc10c590858a8a761540fee858
eggstrom <tristan65536@gmail.com>
2024-12-07 12:14:00 +0100

    Refactor lsp.lua

--- modules/required/neovim/home/.config/nvim/lua/plugins/lsp.lua
+++ modules/required/neovim/home/.config/nvim/lua/plugins/lsp.lua
@@ -1,63 +1,46 @@
-local function settings(extra_settings)
-    extra_settings = extra_settings or {}
-
-    return vim.tbl_extend("force", {
-        capabilities = require("cmp_nvim_lsp").default_capabilities(),
-        on_attach = function(ev)
-            local opts = { buffer = ev.buf }
-            vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-            vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-            vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-            vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-            vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
-            vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
-            vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
-            vim.keymap.set("n", "<leader>wl", function()
-                print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-            end, opts)
-            vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
-            vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, opts)
-            vim.keymap.set({ "n", "v" }, "<leader>a", vim.lsp.buf.code_action, opts)
-            vim.keymap.set("", "<leader>F", function()
-                vim.lsp.buf.format({
-                    async = true,
-                    filter = function(client)
-                        local disabled = { "cssls", "clangd", "html", "jdtls", "jsonls", "lua_ls", "ts_ls" }
-                        for _, lsp in ipairs(disabled) do
-                            if client.name == lsp then
-                                return false
-                            end
+local shared_config = {
+    capabilities = require("cmp_nvim_lsp").default_capabilities(),
+    on_attach = function(ev)
+        local opts = { buffer = ev.buf }
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+        vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+        vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
+        vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
+        vim.keymap.set("n", "<leader>wl", function()
+            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+        end, opts)
+        vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
+        vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, opts)
+        vim.keymap.set({ "n", "v" }, "<leader>a", vim.lsp.buf.code_action, opts)
+        vim.keymap.set("", "<leader>F", function()
+            vim.lsp.buf.format({
+                async = true,
+                filter = function(client)
+                    local disabled = { "cssls", "clangd", "html", "jdtls", "jsonls", "lua_ls", "ts_ls" }
+                    for _, lsp in ipairs(disabled) do
+                        if client.name == lsp then
+                            return false
                         end
-                        return true
-                    end,
-                })
-            end, opts)
-        end,
-    }, extra_settings)
+                    end
+                    return true
+                end,
+            })
+        end, opts)
+    end,
+}
+
+local function lspconfig_setup(server_configs)
+    local lspconfig = require("lspconfig")
+    for server, config in pairs(server_configs) do
+        lspconfig[server].setup(vim.tbl_extend("force", shared_config, config))
+    end
 end
 
--- Configure LSPs
-local lspconfig = require("lspconfig")
--- Bash
-lspconfig.bashls.setup(settings())
--- C, C++
-lspconfig.clangd.setup(settings({
-    -- Force offset encoding to disable warnings
-    cmd = { "clangd", "--offset-encoding=utf-16" },
-}))
--- CSS
-lspconfig.cssls.setup(settings())
--- Dart
-lspconfig.dartls.setup(settings())
--- Go
-lspconfig.gopls.setup(settings())
--- HTML
-lspconfig.html.setup(settings())
--- Java
-lspconfig.jdtls.setup(settings())
--- JavaScript, TypeScript
-local inlay_hint_settings = {
+local ts_ls_inlay_hint_settings = {
     includeInlayEnumMemberValueHints = true,
     includeInlayFunctionLikeReturnTypeHints = true,
     includeInlayFunctionParameterTypeHints = true,
@@ -66,53 +49,55 @@ local inlay_hint_settings = {
     includeInlayPropertyDeclarationTypeHints = true,
     includeInlayVariableTypeHints = true,
 }
-lspconfig.ts_ls.setup(settings({
-    settings = {
-        javascript = { inlayHints = inlay_hint_settings },
-        typescript = { inlayHints = inlay_hint_settings },
+
+lspconfig_setup({
+    bashls = {}, -- Bash
+    clangd = { -- C, C++
+        -- Force offset encoding to disable warnings
+        cmd = { "clangd", "--offset-encoding=utf-16" },
     },
-}))
--- JSON
-lspconfig.jsonls.setup(settings())
--- Lua
-lspconfig.lua_ls.setup(settings({
-    settings = {
-        Lua = {
-            hint = { enable = true },
-            completion = { callSnippet = "Replace" },
-            runtime = { version = "LuaJIT" },
-            workspace = {
-                checkThirdParty = false,
-                library = { vim.env.VIMRUNTIME },
-            },
+    cssls = {}, -- CSS
+    dartls = {}, -- Dart
+    gopls = {}, -- Go
+    html = {}, -- HTML
+    jdtls = {}, -- Java
+    ts_ls = { -- JavaScript, TypeScript
+        settings = {
+            javascript = { inlayHints = ts_ls_inlay_hint_settings },
+            typescript = { inlayHints = ts_ls_inlay_hint_settings },
         },
     },
-    on_init = function()
-        require("lazydev").setup()
-    end,
-}))
--- Markdown
-lspconfig.marksman.setup(settings())
--- Nix
-lspconfig.nixd.setup(settings())
--- Python
-lspconfig.pyright.setup(settings())
--- Rust
-lspconfig.rust_analyzer.setup(settings())
--- TOML
-lspconfig.taplo.setup(settings())
--- VimScript
-lspconfig.vimls.setup(settings())
--- XML
-lspconfig.lemminx.setup(settings())
--- YAML
-lspconfig.yamlls.setup(settings())
--- Zig
-lspconfig.zls.setup(settings({
-    on_init = function()
-        vim.g.zig_fmt_autosave = 0
-    end,
-}))
+    jsonls = {}, -- JSON
+    lua_ls = { -- Lua
+        settings = {
+            Lua = {
+                hint = { enable = true },
+                completion = { callSnippet = "Replace" },
+                runtime = { version = "LuaJIT" },
+                workspace = {
+                    checkThirdParty = false,
+                    library = { vim.env.VIMRUNTIME },
+                },
+            },
+        },
+        on_init = function()
+            require("lazydev").setup()
+        end,
+    },
+    marksman = {}, -- Markdown
+    nixd = {}, -- Nix
+    pyright = {}, -- Python
+    rust_analyzer = {}, -- Rust
+    taplo = {}, -- TOML
+    vimls = {}, -- VimScript
+    lemminx = {}, -- XML
+    yamlls = {}, -- YAML
+    zls = { -- Zig
+        on_init = function()
+            vim.g.zig_fmt_autosave = 0
+        end,
+    },
+})
 
 -- Configure formatters
 local null_ls = require("null-ls")
