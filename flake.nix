7c55844eaca141ed09ae39c4ed354bf9fcbd8313
eggstrom <tristan65536@gmail.com>
2025-01-26 11:03:06 +0100

    Bump nixpkgs-stable to 24.11

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "nixpkgs/nixos-24.05";
+    nixpkgs-stable.url = "nixpkgs/nixos-24.11";
 
     home-manager = {
       url = "github:nix-community/home-manager";
