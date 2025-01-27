9b553e3ceb9bc899edad50b755364ce703cc1ead
eggstrom <tristan65536@gmail.com>
2025-01-27 13:38:19 +0100

    Replace runCommand with runCommandLocal in starship config

--- home/starship.nix
+++ home/starship.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 {
   programs.starship.enable = true;
-  xdg.configFile."starship.toml".source = pkgs.runCommand "starship.toml" { } ''
+  xdg.configFile."starship.toml".source = pkgs.runCommandLocal "starship.toml" { } ''
     echo 'add_newline = false' > "$out"
     ${pkgs.starship}/bin/starship preset nerd-font-symbols >> "$out"
   '';
