303944c1052d7a8af0d4e3bd55af202ef1cf915c
eggstrom <tristan65536@gmail.com>
2025-01-31 12:44:07 +0100

    Readd eww mpd module

--- home/eww/default.nix
+++ home/eww/default.nix
@@ -10,11 +10,25 @@ lib.mkIf (!config.userConfig.minimal) {
     brightnessctl
     eww
     luajit
+    mpc-cli
     pamixer
     pulseaudio
     pulsemixer
     xdotool
   ];
+
+  services = {
+    mpd = {
+      enable = true;
+      musicDirectory = "${config.xdg.userDirs.music}/playlist";
+      extraConfig = ''
+        audio_output {
+          type "pipewire"
+          name "pipewire"
+        }
+      '';
+    };
+  };
 
   xdg.configFile.eww = {
     source = ./home/.config/eww;
