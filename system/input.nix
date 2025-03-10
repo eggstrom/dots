e763bd29ba63449a007948ff24e436efcf0c439d
eggstrom <tristan65536@gmail.com>
2025-03-10 14:16:34 +0100

    Replace keyd with custom tty keymap and setxkbmap

--- system/input.nix
+++ system/input.nix
@@ -1,4 +1,13 @@
+{ pkgs, ... }:
 {
+  console.keyMap = pkgs.writeText "custom.map" ''
+    include "${pkgs.kbd}/share/keymaps/i386/qwerty/us.map.gz"
+
+    # Swap Ctrl with Caps Lock
+    keycode 29 = Caps_Lock
+    keycode 58 = Control
+  '';
+
   services.libinput.mouse = {
     # Disable acceleration
     accelProfile = "flat";
@@ -6,33 +15,5 @@
     middleEmulation = false;
     # Disable delay when clicking (I think)
     tappingDragLock = false;
-  };
-
-  services.keyd = {
-    enable = true;
-
-    keyboards.default = {
-      ids = [ "*" ];
-      settings = {
-        global.overload_tap_timeout = 200;
-        main = {
-          capslock = "overload(control, esc)";
-          rightalt = "layer(rightalt)";
-        };
-      };
-
-      # These need to be in extraConfig because their order matters
-      extraConfig = ''
-        [rightalt:A]
-        c = č
-        s = š
-        z = ž
-
-        [rightalt+shift]
-        c = Č
-        s = Š
-        z = Ž
-      '';
-    };
   };
 }
