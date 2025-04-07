19cf2b26533f6ad35db6b06fc9db3c559b153ac5
eggstrom <tristan65536@gmail.com>
2025-04-07 04:34:29 +0200

    Add GNUPGHOME and PASSWORD_STORE_DIR to xdg.sh

--- files/home/.config/bash/xdg.sh
+++ files/home/.config/bash/xdg.sh
@@ -18,10 +18,12 @@ export XINITRC="$XDG_CONFIG_HOME/xinitrc"             # .xinitrc
 # .local/share/
 export CARGO_HOME="$XDG_DATA_HOME/cargo"                    # .cargo/
 export DOTNET_CLI_HOME="$XDG_DATA_HOME/dotnet"              # .dotnet/, .nuget/, .templateengine/
+export GNUPGHOME="$XDG_DATA_HOME/gnupg"                     # .gnupg/
 export GOPATH="$XDG_DATA_HOME/go"                           # go/
 export GRADLE_USER_HOME="$XDG_DATA_HOME/gradle"             # .gradle/
 export HISTFILE="$XDG_DATA_HOME/bash_history"               # .bash_history
 export NODE_REPL_HISTORY="$XDG_DATA_HOME/node_repl_history" # .node_repl_history
+export PASSWORD_STORE_DIR="$XDG_DATA_HOME/password-store"   # .password-store/
 export PYTHON_HISTORY="$XDG_DATA_HOME/python_history"       # .python_history
 export RUSTUP_HOME="$XDG_DATA_HOME/rustup"                  # .rustup/
 export WINEPREFIX="$XDG_DATA_HOME/wine"                     # .wine/
