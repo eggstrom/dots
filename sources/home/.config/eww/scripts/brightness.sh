fcd7a59147a970d1e67510a9d0a3b497d1b5ad93
eggstrom <tristan65536@gmail.com>
2025-04-30 06:29:16 +0200

    Remove eww config

--- sources/home/.config/eww/scripts/brightness.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Prints "true" and updates brightness if backlight is found, otherwise prints "false"
-if brightnessctl -c backlight >/dev/null 2>&1; then
-  eww update brightness="$(("$(brightnessctl get)" * 100 / "$(brightnessctl max)"))"
-  echo true
-else
-  echo false
-fi
